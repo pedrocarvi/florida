9a759282fdef76c064e08970d771f6820956f4a7
Pedro Carvi <pedro.carvi-11@outlook.es>
2021-11-29 22:41:21 -0300

    Probando firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './ItemListContainer.css'
 import ItemList from './ItemList'
+import { getFirestore } from '../../services/getFirestore.js';
 import Spinner from './Spinner';
 import { productos } from '../../services/productos';
 
@@ -21,7 +22,9 @@ const getFetch = new Promise((resolve, reject) => {
 
 export default function ItemListContainer({greeting}) {
 
+    const [bool, setBool] = useState(true)
     const [products, setProducts] = useState([])
+    const [prod, setProd] = useState({})
     const [loading, setLoading] = useState(true)
 
         
@@ -29,30 +32,43 @@ export default function ItemListContainer({greeting}) {
 
     // Usamos el useEffect para que se llame una sola vez. Clase 6 : 1h 37min 
     useEffect(() => {
-        
-        if ( categoryID ) {
 
-            getFetch
-            .then (res => {
-                setProducts(res.filter(prod => prod.categoria === categoryID))    
-            })
-            .catch( err => {
-                console.log(`Hubo un error ${err}`);
-            })
-            .finally(() => setLoading(false))
+        const dbQuery = getFirestore()
 
-        } else {
+        // TRAER UN PRODUCTO POR SU ID
+        // dbQuery.collection('items').doc('3dZbnnSOwlULKjElhePn').get()
+        // .then(resp => setProd( { id: resp.id,  ...resp.data() } ))
+
+        // TRAER TODOS
+        dbQuery.collection('items').where('precio', '<', 1000).get()
+        .then(data => setProducts(data.docs.map(produ => ( { id: produ.id,  ...produ.data() }))))
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+
+        console.log(products);
+        // if ( categoryID ) {
+
+        //     getFetch
+        //     .then (res => {
+        //         setProducts(res.filter(prod => prod.categoria === categoryID))    
+        //     })
+        //     .catch( err => {
+        //         console.log(`Hubo un error ${err}`);
+        //     })
+        //     .finally(() => setLoading(false))
+
+        // } else {
             
-            getFetch
-            .then (res => {
-                setProducts(res)    
-            })
-            .catch( err => {
-                console.log(`Hubo un error ${err}`);
-             })
-            .finally(() => setLoading(false))
+        //     getFetch
+        //     .then (res => {
+        //         setProducts(res)    
+        //     })
+        //     .catch( err => {
+        //         console.log(`Hubo un error ${err}`);
+        //      })
+        //     .finally(() => setLoading(false))
         
-        }
+        // }
         
     }, [categoryID] )
 
---
+++ src/services/getFirestore.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyADG_edGp3Hsl7JLFTxUwPN1Th0pM7HGpo",
+    authDomain: "florida-e-commerce.firebaseapp.com",
+    projectId: "florida-e-commerce",
+    storageBucket: "florida-e-commerce.appspot.com",
+    messagingSenderId: "1060770086346",
+    appId: "1:1060770086346:web:aad396b6b0d86e4e8ae333"
+  };
+
+  const app = firebase.initializeApp(firebaseConfig)
+
+  export function getFirestore() {
+      return firebase.firestore(app)
+  }
